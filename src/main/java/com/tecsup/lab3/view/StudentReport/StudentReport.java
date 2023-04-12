3b35ce6b9a003bbc166dce8cd53d8ee596cd380d
Maxim <Maxim@192.168.1.7>
2023-04-11 22:18:18 -0500

    Agregando StudentReport

---
+++ src/main/java/com/tecsup/lab3/view/StudentReport/StudentReport.java
@@ -0,0 +1,9 @@
+package com.tecsup.lab3.view.StudentReport;
+
+public class StudentReport {
+	
+		public static void main( String[] args )
+	    {
+	        System.out.println( "Student Report!" );
+	    }
+}
