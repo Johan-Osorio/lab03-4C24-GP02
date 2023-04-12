39f35ed9153c98eeddb63e1808c059a4505590d2
Sejel96 <eber.solano@tecsup.edu.pe>
2023-04-11 21:13:34 -0500

    clase modelStudent agregada

---
+++ src/main/java/com/tecsup/lab3/model/Student/modelStudent.java
@@ -0,0 +1,9 @@
+package com.tecsup.lab3.model.Student;
+
+public class modelStudent {
+	
+	 public static void main( String[] args )
+	    {
+	        System.out.println( "modelStudent!" );
+	    }
+}
